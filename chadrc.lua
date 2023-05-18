8f46163c6285ed9ec291f7f63d9452cbb99dbf44
wuyuanqing <wuyuanqing2@huawei.com>
2023-05-18 11:29:04 +0800

    config(theme): 修改了两个默认主题, github-light的透明模式非常糟糕.

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = {"catppuccin", "github_light" },
+  theme = "ayu_light",
+  theme_toggle = {"ayu_light", "yoru" },
   transparency = false,
 
   hl_override = highlights.override,
