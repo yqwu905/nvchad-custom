b1c02890be447c7e3c1b5af7cfdf134cf7f577ee
wuyuanqing <wuyuanqing2@huawei.com>
2023-05-18 11:33:09 +0800

    plugins: 添加Git blame

--- plugins.lua
+++ plugins.lua
@@ -186,6 +186,38 @@ local plugins = {
       user = "w30044123",
     },
   },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    ft = { "gitcommit", "diff" },
+    dependencies = {
+        "f-person/git-blame.nvim",
+    },
+    init = function()
+      -- load gitsigns only when a git file is opened
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+            vim.schedule(function()
+              require("lazy").load { plugins = { "gitsigns.nvim" } }
+            end)
+          end
+        end,
+      })
+      require("core.utils").load_mappings "gitsigns"
+    end,
+    opts = function()
+      return require("plugins.configs.others").gitsigns
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "git")
+      require("gitsigns").setup(opts)
+    end,
+  },
+
 }
 
 return plugins
