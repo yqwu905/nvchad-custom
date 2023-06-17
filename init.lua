d3a57e113adb3a255a122d4d0286895c94fb12a7
yqwu905 <pingline@outlook.com>
2023-06-17 20:45:13 +0800

    feat(system): add support for clipboard in wsl

--- init.lua
+++ init.lua
@@ -35,3 +35,18 @@ if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = "railgun"
     vim.o.guifont = "JetBrainsMono Nerd Font:h12"
 end
+
+if vim.fn.has "wsl" then
+    vim.g.clipboard = {
+        name = "win32yank",
+        copy = {
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
+        },
+        cache_enabled = 0,
+    }
+end
