ac2e9af5404d104476ebbd52548a274d5023fa87
yqwu905 <pingline@outlook.com>
2023-06-17 21:22:46 +0800

    feat(utils): Support determine script extensions and runner by system type

--- plugins.lua
+++ plugins.lua
@@ -108,13 +108,17 @@ local plugins = {
         cmd = { "ToggleTerm" },
         opts = {},
         config = function(_, opts)
-            vim.opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
-            vim.opt.shellcmdflag =
-                "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-            vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-            vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-            vim.opt.shellquote = ""
-            vim.opt.shellxquote = ""
+            if vim.fn.has "linux" then
+                vim.opt.shell = "zsh"
+            else
+                vim.opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
+                vim.opt.shellcmdflag =
+                    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+                vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+                vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+                vim.opt.shellquote = ""
+                vim.opt.shellxquote = ""
+            end
             require("toggleterm").setup(opts)
         end,
     },
