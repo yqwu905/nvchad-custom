faf78a99f95b53e23b3080105ea68d0af70c1ec1
wuyuanqing <wuyuanqing2@huawei.com>
2023-07-11 17:18:42 +0800

    Revert "feat(plugins): Use flash.nvim instead of hop.nvim"
    This reverts commit cf2cc445f05767745a80ff0d36a0df2a177f1027.

--- plugins.lua
+++ plugins.lua
@@ -73,6 +73,12 @@ local plugins = {
     },
 
     {
+        "phaazon/hop.nvim",
+        cmd = { "HopWord" },
+        opts = {},
+    },
+
+    {
         "numToStr/Comment.nvim",
         keys = { "gc", "gb" },
         init = function()
@@ -224,54 +230,6 @@ local plugins = {
         config = function(_, opts)
             require("project_nvim").setup(opts)
         end,
-    },
-
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        opts = {},
-        keys = {
-            {
-                "s",
-                mode = { "n", "x", "o" },
-                function()
-                    require("flash").jump()
-                end,
-                desc = "Flash",
-            },
-            {
-                "S",
-                mode = { "n", "o", "x" },
-                function()
-                    require("flash").treesitter()
-                end,
-                desc = "Flash Treesitter",
-            },
-            {
-                "r",
-                mode = "o",
-                function()
-                    require("flash").remote()
-                end,
-                desc = "Remote Flash",
-            },
-            {
-                "R",
-                mode = { "o", "x" },
-                function()
-                    require("flash").treesitter_search()
-                end,
-                desc = "Flash Treesitter Search",
-            },
-            {
-                "<c-s>",
-                mode = { "c" },
-                function()
-                    require("flash").toggle()
-                end,
-                desc = "Toggle Flash Search",
-            },
-        },
     },
 }
 
